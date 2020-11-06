8f2ea0e7c2e8f3ee6028970cc1d83d3b2fc0ce5b
Valera <vc750870@gmail.com>
2020-11-06 17:02:53 +0200

    added product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,21 @@ $(function(){
 
   var mixer = mixitup('.products__wrap-box');
 
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px"
+    // readOnly: true
+  });
+
+  $('.product-slider__wrap').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    dots: true,
+    arrows: false
+
+  });
+
 
 
 
