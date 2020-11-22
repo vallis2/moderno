8313c8544bb324f3c0ce4fdfcec10d42075875a4
Valera <vc750870@gmail.com>
2020-11-22 15:51:28 +0200

    added adaptiv header, footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,34 @@
 $(function(){
 
-  var mixer = mixitup('.products__wrap-box');
-
+  
   $(".rate-star").rateYo({
     rating: 5,
     starWidth: "12px"
     // readOnly: true
   });
-
+  
   $('.product-slider__wrap').slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 2,
     dots: true,
     arrows: false
-
+    
   });
 
+  // menu__btn
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
 
+  $('.header__btm-menu').on('click', function(){
+    $('.header__box').toggleClass('active');
+  });
+  
 
+  
+  
+
+  var mixer = mixitup('.products__wrap-box');
 
 });
